187d61ee45b7a7e8fd06716e10f0a7add1492d55
Chris Van Huffe <cvanhuffel@outlook.com>
2021-11-11 19:59:18 -0800

    revStr file added, README updated

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 A workspace for Javascript rudiments. 
 
 Rudiments: https://en.wikipedia.org/wiki/Drum_rudiment
+
+*NOTE* until I can set up unit testing, I am running tests for each code block in my local console. 
---
+++ revStr.js
@@ -0,0 +1,31 @@
+// Reverse a String With a Decrementing For Loop
+
+// *Step 1 - Create empty string that will contain newly created string
+// *Step 2 - Create FOR LOOP , the starting point of the loop will be (str.length -1) which corresponds to the last char of the string "s"
+// As long as i is >= 0, the loop will continue, we decrement i after each iteration
+// * Step 3 - Return the reversed string
+// For more info see: https://www.freecodecamp.org/news/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb/
+
+function revString(str) {
+  let newString = ""; // *Step 1
+  // *Step 2
+  for(let i = str.length - 1; i >= 0; i--) {
+    newString +=str[i];
+  }
+  return newString; // *Step 3
+}
+revString('mycatrules'); // Expected output: selurtacym
+
+// Using ES6 FOR OF method - more notes on this coming soon
+
+function revStr(str) {
+  let reversed = "";
+  for(let char of str){
+    reversed = char + reversed;
+  }
+  return reversed; // Expected output: olleh
+}
+
+const output = revStr('hello');
+
+console.log(output);
