727d58c06f6d1b10dc56aa9f3bc1781b6682a1ea
Chris Van Huffe <cvanhuffel@outlook.com>
2021-12-05 17:20:10 -0800

    Updated find file

--- find.js
+++ find.js
@@ -31,11 +31,39 @@ const output = comment;
 // findIndex method
 // Same syntax as FIND, replacing FIND w/findIndex
 // Find the comment with this ID
-// delete the comment with the ID of 823423
 
 const index = comments.findIndex(comment => comment.id === 823423);
 
 const output2 = index;
 // Expected output: 1 (second position in array)
 
-console.table(output2);
+// Delete the comment with the ID of 823423(calling .splice method)
+// comments.splice(index, 1);
+
+// Delete a comment (with the ID of 823423) and return a new array of updated comments using .slice method and SPREAD OP
+
+// Step 1: Create new VAR array
+// Step 2: Using SPREAD syntax(...), copy original array (passing in 0 and index)
+// Step 3: Build a new array, passing in index plus 1
+
+// *1 ↓
+const newComments = [
+  ...comments.slice(0, index),  // *2
+  ...comments.slice(index + 1)  // *3
+];
+
+console.table(newComments);
+// // Expected output:   ┌─────────┬───────────────────────────────┬─────────┐
+// │ (index) │             text              │   id    │
+// ├─────────┼───────────────────────────────┼─────────┤
+// │    0    │         'Love this!'          │ 523423  │
+// │    1    │      'You are the best'       │ 2039842 │
+// │    2    │ 'Chicken is my fav food ever' │ 123523  │
+// │    3    │       'Nice Nice Nice!'       │ 542328  │
+// └─────────┴───────────────────────────────┴─────────┘
+
+// For more info on .splice() method: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+
+// For more info on .slice() method: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+
+// For more info on SPREAD syntax(...): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
