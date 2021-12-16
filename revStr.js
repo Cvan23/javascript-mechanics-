948d35c09a0d53936211ec0746b71bf408fc3b3f
Chris Van Huffe <cvanhuffel@outlook.com>
2021-12-15 17:47:55 -0800

    Added notes to revStr file

--- revStr.js
+++ revStr.js
@@ -1,9 +1,10 @@
 // Reverse a String With a Decrementing For Loop
 
-// *Step 1 - Create empty string that will contain newly created string
-// *Step 2 - Create FOR LOOP , the starting point of the loop will be (str.length -1) which corresponds to the last char of the string "s"
+// *Step 1: Create empty string that will contain newly created string
+// *Step 2: Create FOR LOOP , the starting point of the loop will be (str.length -1) which corresponds to the last char of the string "s"
 // As long as i is >= 0, the loop will continue, we decrement i after each iteration
-// * Step 3 - Return the reversed string
+// Step 3: Inside FOR LOOP: add str[i] to new string using addition assignment OP (+=)
+// * Step 4: Return the reversed string
 // For more info see: https://www.freecodecamp.org/news/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb/
 
 function revString(str) {
@@ -11,9 +12,9 @@ function revString(str) {
 
       // *Step 2 ↓
   for(let i = str.length - 1; i >= 0; i--) {
-    newString +=str[i];
+    newString +=str[i]; // *Step 3
   }
-  return newString; // *Step 3
+  return newString; // *Step 4
 }
 revString('mycatrules'); // Expected output: selurtacym
 
